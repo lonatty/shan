a9874a9a76519d6a46c3d6b8e7631f9b7b0891ea
loanatty <127379495+lonatty@users.noreply.github.com>
2023-06-18 16:48:01 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+//go:build go1.19
+
+//go:generate go run . completion bash -o completions/chezmoi-completion.bash
+//go:generate go run . completion fish -o completions/chezmoi.fish
+//go:generate go run . completion powershell -o completions/chezmoi.ps1
+//go:generate go run . completion zsh -o completions/chezmoi.zsh
+//go:generate go run ./internal/cmds/generate-install.sh -o assets/scripts/install.sh
+
+package main
+
+import (
+	"os"
+
+	"github.com/twpayne/chezmoi/v2/pkg/cmd"
+)
+
+var (
+	version string
+	commit  string
+	date    string
+	builtBy string
+)
+
+func main() {
+	if exitCode := cmd.Main(cmd.VersionInfo{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+		BuiltBy: builtBy,
+	}, os.Args[1:]); exitCode != 0 {
+		os.Exit(exitCode)
+	}
+}
